66d9a16a2956756fea2bba92f9c3723fb5118c17
changhong <changhongwang@126.com>
2018-09-19 09:28:08 +0800

    bug修复，原因为reducer中set值错误

--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -7,10 +7,10 @@ const defaultState = fromJS({
 
 export default (state = defaultState, action) => {
   if (action.type === constants.SEARCH_FOCUS) {
-    return state.set('focus', true);
+    return state.set('focused', true);
   }
   if (action.type === constants.SEARCH_BLUR) {
-    return state.set('focus', false);
+    return state.set('focused', false);
   }
   return state;
 }
