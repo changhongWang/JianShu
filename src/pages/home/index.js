3c616c4740c66fa3f0be16e6a599bd61bc7b8776
changhong.wang <changhong.wang@qunar.com>
2018-10-07 17:02:20 +0800

    首页性能优化-pureComponent

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import {HomeWrapper, HomeLeft, HomeRight} from './style';
 import List from './components/List';
 import Recommend from './components/Recommend';
@@ -9,7 +9,7 @@ import {getHomeInfo} from './store/actionCreators';
 import {actionCreators} from './store'
 import { BackTop } from './style';
 
-class Home extends Component {
+class Home extends PureComponent {
   handleScrollTop () {
     window.scrollTo(0, 0);
   }
