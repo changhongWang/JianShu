7500ace25366883b91a8934c3b212f085797f7c2
changhong.wang <changhong.wang@qunar.com>
2018-10-14 16:27:22 +0800

    登录页布局

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './common/header';
 import Home from './pages/home';
+import Login from './pages/login';
 import Detail from './pages/detail';
 import store from './store';
 
@@ -15,8 +16,9 @@ class App extends Component {
         <BrowserRouter>
           <div>
             <Header/>
-            <Route path="/" exact component={Home}></Route>
-            <Route path="/detail/:id" exact component={Detail}></Route>
+            <Route path='/' exact component={Home}></Route>
+            <Route path='/login' exact component={Login}></Route>
+            <Route path='/detail/:id' exact component={Detail}></Route>
           </div>
         </BrowserRouter>
       </Provider>
